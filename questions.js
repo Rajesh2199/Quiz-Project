6811f62e8f7299afbeb80dddcdb75d26c6ab4512
baduraje@isu.edu <baduraje@isu.edu>
2020-06-20 12:25:30 -0600

    updates

---
+++ questions.js
@@ -0,0 +1,64 @@
+var questions = [{
+    "question": "What does VAR stand for?",
+    "option1": "Variance",
+    "option2": "Variation",
+    "option3": "Value",
+    "option4": "Variable",
+    "answer":  "4"
+},{
+    "question": "Which of the following denotes Exponentiation operator?",
+    "option1": "#",
+    "option2": "**",
+    "option3": "***",
+    "option4": "*",
+    "answer":  "2"
+}, {
+    "question": "Which of the following is ternary operator?",
+    "option1": "??",
+    "option2": "?",
+    "option3": "???",
+    "option4": "????",
+    "answer":  "2"
+},{
+    "question": "Inside which HTML element do we put the JavaScript?",
+    "option1": "<javascript>",
+    "option2": "<scripting>",
+    "option3": "<js>",
+    "option4": "<Script>",
+    "answer":  "4"
+},{
+    "question": "Which of the following denotes strict Equal?",
+    "option1": "==",
+    "option2": "====",
+    "option3": "==",
+    "option4": "===",
+    "answer":  "4"
+}
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
